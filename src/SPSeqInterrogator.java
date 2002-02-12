4c4dc82c4e772ce750c43f592009592d7d464440
Sharon Giannatto <Sharon.Giannatto@jax.org>
2002-02-12 18:31:36 +0000

    Added isOrganismClassif method to test for criteria in the EMBL-format OS section

--- src/SPSeqInterrogator.java
+++ src/SPSeqInterrogator.java
@@ -7,13 +7,18 @@ public class SPSeqInterrogator
 {
 	// Concept:
         //        IS: an object that queries a SwissProt/TREMBL sequence record 
-	//	      to determine if it is for a given organism
         //       HAS: a mapping structure for mapping controlled vocabulary
 	//	      to string expressions	
         //      DOES: Given a sequence record and a controlled vocabulary string
-	//		determine if a predicate is true. e.g. Given a sequence
-	//		record 's' and a string "mouse" determine if 's' is a 
-	//		mouse record 
+	//		determine if 's' is for a organism 'organism' . 
+	//		e.g. Given sequence record 's' and controlled vocab 
+	//		string "mouse" determine if 's' is a mouse record. 
+	//            Given a sequence record and a controlled vocabulary string
+	// 		determine if 's' is organism classification for 
+	//              'organism' 
+	//		e.g. Given a sequence record 's' and controlled vocab
+	//		string "rodentia" determine if 's' has "rodentia" 
+	//		organism classification
 	// RESPONSIBLE FOR: 1) mapping controlled vocab terms to string 
 	//		       expressions
 	//		    2) providing basic predicates to compare fields
@@ -45,6 +50,30 @@ public class SPSeqInterrogator
                 else
                         return false;
 	}
+	
+	public boolean isOrganismClassif(
+                SeqRecord s,       // a SwissProt sequence record
+                String organism)   // organism controlled vocabulary
+        {
+        // Purpose: Determines whether sequence record 's' has classification
+	//           for 'organism'
+        // Returns: boolean true or false
+        // Assumes: 'organism' is a valid controlled vocabulary for 's'
+        //              and has been converted to lower case
+        // Effects: nothing
+        // Throws: nothing
+        // Notes:
+
+                // get the string expression that is mapped to 'organism'
+                String matchString = (String)expressions.get(organism);
+
+                // return true if the string expression matches organism of  's'
+                if((((EMBLSeqRecord)s).getOrganismClassif()).indexOf(matchString) >  -1)
+                        return true;
+                else
+                        return false;
+        }
+
 	
 	//
 	// instance variables
