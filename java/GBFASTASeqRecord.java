c04daab1aff7aa279e7048dbffe0929337bcecab
David Miers <dave.miers@jax.org>
2006-03-06 14:46:55 +0000

    Moved from src directory

---
+++ java/GBFASTASeqRecord.java
@@ -0,0 +1,87 @@
+package org.jax.mgi.bio.seqrecord;
+
+import java.io.*;
+import java.util.*;
+
+public class GBFASTASeqRecord extends FASTASeqRecord
+{
+	// Concept:
+	//	  IS: an object that represents a GBFASTA-format sequence record
+	//	 HAS: see superclass
+	//	DOES: Extends the super class readText method to parse out the
+	//	      seqId from the description
+	//	      Also see superclass
+	// Implementation:
+
+	//
+	// Constructors:
+	//
+
+        public GBFASTASeqRecord()
+        {
+	    // Purpose: Creates a default GBFASTASeqRecord object.
+	    // Throws: nothing
+
+		super();
+
+        }
+
+	//
+	// Methods:
+        //
+        //
+
+	public void readText(BufferedReader reader)
+		throws IOException, EOFException
+        {
+        // Purpose: reads a FASTA-format sequence record extending the super
+	//          class method by parsing the GenBank seqId out of the Genbank
+	//          FASTA 'unique identifier'
+        // Returns: nothing
+        // Assumes: "reader" is a stream of FASTA-format sequence records
+        // Effects: "reader" has advanced to the next record in the stream
+        // Throws: IO, EOF,  and regular expression syntax exceptions
+
+		// have the superclass method do its work
+		super.readText(reader);
+
+		// for discarded tokens
+		String dummy;
+
+		// carriage return
+                String CRT = "\n";
+		// the seqidversion token in the description line
+		int seqIdVersionToken = 4;
+
+		// tokenized FASTA Id - a FASTA Id is a unique identifier that
+		// contains a GenBank SeqId which we must parse out e.g.
+		// this.seqIds.get(0) has the following format
+		// gi|3287367|gb|AC002397.1|AC002397
+		// we must parse out the last token tokenized on '|'
+		// Jan 03 - note the last token is not always the seqid
+		// thi 4th token is seqIdVersion - get it than tokenize it on
+		// '.' to get the seqId
+		StringTokenizer tokenizedFASTAId = new StringTokenizer(
+                        (String)(this.seqIds.get(0)), "|");
+
+		// count of total tokens in tokenizedFASTAId
+		int count = seqIdVersionToken;
+
+		// the last token is the seqId
+		for(int i = 1; i < count ; i++)
+		{
+			dummy = tokenizedFASTAId.nextToken();
+		}
+
+		String seqIdVersion = tokenizedFASTAId.nextToken();
+
+		// to extract the seqId from seqIdVersion
+                StringTokenizer tokenizedSeqIdV = new StringTokenizer(
+			seqIdVersion, ".");
+		// seqId is the first token
+		this.seqIds.set(0, tokenizedSeqIdV.nextToken());
+
+	}
+
+}
+
---
+++ java/SeqRecord.java
@@ -0,0 +1,148 @@
+package org.jax.mgi.bio.seqrecord;
+
+import java.io.*;
+import java.util.*;
+
+public abstract class SeqRecord
+{
+	// Concept:
+	//	  IS: an abstract representation of a sequence record which
+	//		expects its concrete subclasses to implement the
+	//		readText method to read/parse specific record types
+	//	 HAS: an organism, division, type (mRNA, DNA, etc),
+	//            sequence, seqIds, seqIdVersion, sequence length, date
+	//	DOES: reads itself from an input stream parsing out the items
+	// 	      in HAS. Provides accessors for each HAS above
+	// Implementation:
+
+	//
+	// Constructors
+	//
+
+	//
+	// Methods
+	//
+
+	public abstract void readText(
+			BufferedReader reader)  // reader for text of the seq-
+						// uence record
+		throws IOException, EOFException;
+		// Purpose: reads itself (a sequence record) from 'reader'
+		// Returns: nothing
+		// Assumes: it will be implemented in all subclasses
+		// Effects: nothing
+		// Throws: IO, EOF,  and regular expression syntax exceptions
+		// Notes:
+
+	public String getLine()
+		// Purpose: accessor for last line read of each record.
+		//          Value will be either end-of-record string for
+		// 	     the sequence record OR null if EOF
+	{
+		return this.line;
+	}
+
+	public String getText()
+		// Purpose: accessor for the text of the whole sequence record
+        {
+                return this.text.toString();
+        }
+
+        public String getOrganism()
+		// Purpose: accessor for the sequence record organism
+        {
+		return (this.organism.toString()).toLowerCase();
+        }
+
+	 public String getType()
+		// Purpose: accessor for the sequence record type
+        {
+                return this.type;
+        }
+
+        public String getDivision()
+		// Purpose: accessor for the sequence record division
+        {
+                return this.division;
+        }
+
+        public String getSequence()
+		// Purpose: accessor for the sequence record sequence
+        {
+                return this.sequence.toString();
+        }
+
+        public Vector getSeqIds()
+		// Purpose: accessor for the sequence record seqIds
+		//           returns a Vector of Strings
+        {
+                return this.seqIds;
+        }
+
+	public String getVersion()
+		// Purpose: accessor for the sequence record seqIdVersion
+		//          Returns empty string for records that do not use
+		//          the convention seqId + '.' + version
+	{
+		return this.seqIdVersion;
+	}
+
+	public String getVersionNumber()
+	{
+		// Purpose: accessor for the sequence record version number
+		//          Returns empty string for records that do not use
+		//	    the convention seqId + '.' + version
+		int index = seqIdVersion.indexOf(".");
+		return seqIdVersion.substring(index + 1,
+			seqIdVersion.length());
+	}
+
+	public int getSeqLength()
+		// Purpose: accessor for the sequence length
+	{
+		return this.seqLength;
+	}
+
+	public String getDate()
+		// Purpose: accessor for the sequence record date
+	{
+		return this.date;
+	}
+
+	//
+	//instance vars
+	//
+
+	// null when EOF, else end of record string
+	protected String line = "";
+
+	// The text of one sequence record
+	protected StringBuffer text = new StringBuffer();
+
+	// The length of the sequence contained in the sequence record
+	protected int seqLength = -1;
+
+	// The sequence type (e.g. mRNA)
+	protected String type = "";
+
+	// The databank division for this sequence
+	protected String division = "";
+
+	// The sequence record date
+	protected String date = "";
+
+	// A Vector of Strings representing seqIds for this record.
+	// Position 0 is the primary seqId
+	protected Vector seqIds = new Vector();
+
+	// This field used only by sequence records which version with the
+	// convention - seqId + '.' + version number
+        protected String seqIdVersion = "";
+
+	// The sequence record organism type(s)
+        protected StringBuffer organism = new StringBuffer();
+
+	// The sequence
+        protected StringBuffer sequence = new StringBuffer();
+}
+
